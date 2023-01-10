1fe9a098d44b89ff17f204ab5ae5204823dde39f
Peter Phan <ipeterphan@gmail.com>
2023-01-09 22:03:23 -0500

    Use old cmp config

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -52,23 +52,86 @@ function config.mason(plugin)
 end
 
 function config.cmp()
-  local cmp = require("cmp")
+  local icons = {
+    kind = require("icons").get("kind", false),
+    type = require("icons").get("type", false),
+    cmp = require("icons").get("cmp", false),
+  }
+  -- vim.api.nvim_command([[packadd cmp-tabnine]])
   local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
   end
+
   local has_words_before = function()
     local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
+
+  local border = function(hl)
+    return {
+      { "╭", hl },
+      { "─", hl },
+      { "╮", hl },
+      { "│", hl },
+      { "╯", hl },
+      { "─", hl },
+      { "╰", hl },
+      { "│", hl },
+    }
+  end
+
+  local cmp_window = require("cmp.utils.window")
+
+  cmp_window.info_ = cmp_window.info
+  cmp_window.info = function(self)
+    local info = self:info_()
+    info.scrollable = false
+    return info
+  end
+
+  local compare = require("cmp.config.compare")
+  local lspkind = require("lspkind")
+  local cmp = require("cmp")
+
   cmp.setup({
-    completion = {
-      completeopt = "menu,menuone,noinsert",
+    window = {
+      completion = {
+        border = border("Normal"),
+        max_width = 80,
+        max_height = 20,
+      },
+      documentation = {
+        border = border("CmpDocBorder"),
+      },
     },
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+        compare.offset,
+        compare.exact,
+        compare.score,
+        require("cmp-under-comparator").under,
+        compare.kind,
+        compare.sort_text,
+        compare.length,
+        compare.order,
+      },
+    },
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        local kind = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 50,
+          symbol_map = vim.tbl_deep_extend("force", icons.kind, icons.type, icons.cmp),
+        })(entry, vim_item)
+        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+        kind.kind = " " .. strings[1] .. " "
+        kind.menu = "    (" .. strings[2] .. ")"
+        return kind
       end,
     },
+    -- You can set mappings if you want
     mapping = cmp.mapping.preset.insert({
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
       ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -97,26 +160,22 @@ function config.cmp()
         end
       end, { "i", "s" }),
     }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp", max_item_count = 8 },
-      { name = "luasnip", max_item_count = 8 },
-      { name = "buffer", max_item_count = 8 },
-      { name = "path", max_item_count = 8 },
-      { name = "emoji", max_item_count = 8 },
-    }),
-    formatting = {
-      format = function(_, item)
-        local icons = require("icons").get("kind")
-        if icons[item.kind] then
-          item.kind = icons[item.kind] .. item.kind
-        end
-        return item
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
       end,
     },
-    experimental = {
-      ghost_text = {
-        hl_group = "LspCodeLens",
-      },
+    -- You should specify your *installed* sources.
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "nvim_lua" },
+      { name = "luasnip" },
+      { name = "path" },
+      { name = "spell" },
+      { name = "tmux" },
+      { name = "orgmode" },
+      { name = "buffer" },
+      { name = "latex_symbols" },
     },
   })
 end
@@ -231,7 +290,7 @@ function config.lspsaga()
         local en = node.range["end"]
         if button == "l" then
           if clicks == 2 then
-            -- double left click to do nothing
+          -- double left click to do nothing
           else -- jump to node's starting line+char
             vim.fn.cursor(st.line + 1, st.character + 1)
           end
