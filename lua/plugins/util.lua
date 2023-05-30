6c6f24148d453407dfb7de47f115a68c4fedb7a0
Peter Phan <ipeterphan@gmail.com>
2023-05-29 22:18:10 -0400

    plugin util

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,30 +1,24 @@
 return {
 
-  -- measure startuptime
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    config = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
+	-- measure startuptime
+	{
+		"dstein64/vim-startuptime",
+		cmd = "StartupTime",
+		config = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
 
-  -- session management
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
-    -- stylua: ignore
-    keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-    },
-  },
+	-- session management
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+	},
 
-  -- library used by other plugins
-  { "nvim-lua/plenary.nvim", lazy = true },
+	-- library used by other plugins
+	{ "nvim-lua/plenary.nvim", lazy = true },
 
-  -- makes some plugins dot-repeatable like leap
-  { "tpope/vim-repeat", event = "VeryLazy" },
+	-- makes some plugins dot-repeatable like leap
+	{ "tpope/vim-repeat", event = "VeryLazy" },
 }
