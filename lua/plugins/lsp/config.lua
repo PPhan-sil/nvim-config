cfecf2505dd49625b2a2960ef54114c188854e81
Peter Phan <ipeterphan@gmail.com>
2025-05-14 01:52:12 -0400

    Removed tailwind

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -266,33 +266,6 @@ function config.lsp()
           client.server_capabilities.hoverProvider = false
         end, "ruff")
       end,
-      tailwindcss = function(_, opts)
-        local tw = require("lspconfig.server_configurations.tailwindcss")
-        opts.filetypes = opts.filetypes or {}
-
-        -- Add default filetypes
-        vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-        -- Remove excluded filetypes
-        --- @param ft string
-        opts.filetypes = vim.tbl_filter(function(ft)
-          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-        end, opts.filetypes)
-
-        -- Additional settings for Phoenix projects
-        opts.settings = {
-          tailwindCSS = {
-            includeLanguages = {
-              elixir = "html-eex",
-              eelixir = "html-eex",
-              heex = "html-eex",
-            },
-          },
-        }
-
-        -- Add additional filetypes
-        vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-      end,
     },
   }
 end
