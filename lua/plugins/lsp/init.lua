bfd3f69e109313c2275052289dbce0d3a699c555
Peter Phan <ipeterphan@gmail.com>
2025-11-03 18:05:33 -0500

    Use mainline lsp config because it keeps breaking after updates

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,15 +1,15 @@
 local conf = require("plugins.lsp.config")
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = conf.lsp(),
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "brenoprata10/nvim-highlight-colors", opts = {} },
-    },
-    opts = conf.cmp(),
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   -- opts = conf.lsp(),
+  -- },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     { "brenoprata10/nvim-highlight-colors", opts = {} },
+  --   },
+  --   opts = conf.cmp(),
+  -- },
 }
